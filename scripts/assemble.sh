e29fd59b151968c7412c46923915f0416d35e3c5
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-17 19:57:46 +0300

    Ya student SPBGU

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -5,3 +5,4 @@ SRCDIR="$ROOTDIR/src"
 BUILDDIR="$ROOTDIR/build"
 
 # Расположите свою команду на следующей строке
+gcc -c src/main.c -o build/main.o
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,3 +5,4 @@ SRCDIR="$ROOTDIR/src"
 BUILDDIR="$ROOTDIR/build"
 
 # Расположите свою команду на следующей строке
+gcc -S src/main.c -o build/main.s
